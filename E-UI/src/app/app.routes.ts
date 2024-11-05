ac4bfca84c04554b825226373d776b319ae2d8d0
praveenk545 <kumarp71149@gmail.com>
2024-11-05 10:02:26 +0530

    first commit

---
+++ E-UI/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './componet/header/header.component';
+
+export const routes: Routes = [
+    {
+        path:'header',component:HeaderComponent
+    },
+    
+];
